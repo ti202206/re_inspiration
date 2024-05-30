5fa9c845800de05bfba19362366e9b040bb68f1c
ti202206 <joker999joker@yahoo.co.jp>
2024-05-31 00:41:53 +0900

    お気に入りのダミーデータを登録できるようにした．

--- inspiration/database/seeders/FavoriteSeeder.php
+++ inspiration/database/seeders/FavoriteSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Favorite;
+use App\Models\Idea;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class FavoriteSeeder extends Seeder
@@ -14,6 +16,26 @@ class FavoriteSeeder extends Seeder
      */
     public function run()
     {
-        Favorite::factory()->count(10)->create();
+        // Favorite::factory()->count(10)->create();
+        $users = User::all();
+        $ideas = Idea::all();
+
+        // 各ユーザーに対してランダムなアイデアをお気に入りとして設定
+        foreach ($users as $user) {
+            for ($i = 0; $i < 5; $i++) { // 各ユーザーに対して最大5個のお気に入りを作成
+                $randomIdea = $ideas->random();
+
+                // すでにお気に入りとして登録されているか確認
+                $exists = Favorite::where('user_id', $user->id)->where('idea_id', $randomIdea->id)->exists();
+
+                if (!$exists) {
+                    Favorite::create([
+                        'user_id' => $user->id,
+                        'idea_id' => $randomIdea->id,
+                        'is_favorite' => true
+                    ]);
+                }
+            }
+        }
     }
 }
