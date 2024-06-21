04dc53ebe5caa0cff5fe274564071cd2c592cc9d
ti202206 <joker999joker@yahoo.co.jp>
2024-06-22 02:46:52 +0900

    不要な部分を削除

--- inspiration/public/js/app.js
+++ inspiration/public/js/app.js
@@ -11038,7 +11038,8 @@ __webpack_require__.r(__webpack_exports__);
 // IdeaCard コンポーネント定義
 
 var IdeaCard = function IdeaCard(_ref) {
-  var _ref$idea = _ref.idea,
+  var className = _ref.className,
+    _ref$idea = _ref.idea,
     idea = _ref$idea === void 0 ? {} : _ref$idea,
     _ref$categories = _ref.categories,
     categories = _ref$categories === void 0 ? {} : _ref$categories,
@@ -11060,7 +11061,7 @@ var IdeaCard = function IdeaCard(_ref) {
     return tweetURL;
   };
   return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)("div", {
-    className: "idea-card",
+    className: "idea-card ".concat(className),
     children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)("div", {
       className: "idea-card__content",
       children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("div", {
@@ -11962,9 +11963,6 @@ function IdeaCatalog() {
       });
     }
     if (filterEndDate) {
-      // 終了日の前日まで
-      // const endDate = new Date(filterEndDate);
-      // filtered = filtered.filter(idea => new Date(idea.created_at) <= endDate);
       var endDate = new Date(new Date(filterEndDate).setDate(new Date(filterEndDate).getDate() + 1));
       filtered = filtered.filter(function (idea) {
         return new Date(idea.created_at) < endDate;
@@ -12061,6 +12059,7 @@ function IdeaCatalog() {
           })]
         }), filteredIdeas.length > 0 ? filteredIdeas.map(function (idea) {
           return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx)(_components_IdeaCard__WEBPACK_IMPORTED_MODULE_4__["default"], {
+            className: "idea-card__max-width",
             idea: idea,
             categories: categories,
             isPlaceholder: false,
@@ -12091,38 +12090,6 @@ function IdeaCatalog() {
   });
 }
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (IdeaCatalog);
-
-/* src/components/IdeaCatalog.css */
-
-// .filters {
-//     display: flex;
-//     justify-content: space-between;
-//     margin-bottom: 20px;
-// }
-
-// .filter-item {
-//     flex: 1;
-//     margin-right: 10px;
-// }
-
-// .filter-item:last-child {
-//     margin-right: 0;
-// }
-
-// .filter-item label {
-//     display: block;
-//     margin-bottom: 5px;
-//     font-weight: bold;
-// }
-
-// .filter-item select,
-// .filter-item input {
-//     width: 100%;
-//     padding: 8px;
-//     border: 1px solid #ccc;
-//     border-radius: 4px;
-//     box-sizing: border-box;
-// }
 
 /***/ }),
 
@@ -15338,188 +15305,6 @@ function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) 
 function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
 function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t["return"] && (u = t["return"](), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
 function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-// import Header from "../components/Header";
-// import Footer from "../components/Footer";
-// import { useNavigate } from "react-router-dom";
-
-// function MyReviewHistory() {
-
-//     // const [purchases, setPurchases] = useState([]);
-//     const [user, setUser] = useState(null);
-//     const [categories, setCategories] = useState({});
-//     const [reviewed, setReviewed] = useState([]);
-//     const navigate = useNavigate();
-
-//     const fetchUser = async () => {
-//         try {
-//             const response = await axios.get("/api/user", {
-//                 headers: {
-//                     Authorization: `Bearer ${sessionStorage.getItem(
-//                         "auth_token"
-//                     )}`, // 認証トークンを設定
-//                 },
-//             });
-//             setUser(response.data);
-//             console.log("Fetched user:", response.data);
-//         } catch (error) {
-//             console.error("Error fetching user:", error);
-//         }
-//     };
-
-//     // // 購入済み情報を取得
-//     // const fetchMyPurchases = async () => {
-//     //     try {
-//     //         const response = await axios.get("/api/mypurchases", {
-//     //             headers: {
-//     //                 Authorization: `Bearer ${sessionStorage.getItem(
-//     //                     "auth_token"
-//     //                 )}`,
-//     //             },
-//     //         });
-//     //         const sortedPurchases = response.data.sort(
-//     //             (a, b) => new Date(b.created_at) - new Date(a.created_at)
-//     //         );
-//     //         const recentPurchases = sortedPurchases.slice(0, 5);
-//     //         setPurchases(recentPurchases);
-//     //         console.log("Fetched purchases:", recentPurchases);
-//     //     } catch (error) {
-//     //         console.error("Error fetching purchases:", error);
-//     //     }
-//     // };
-
-//             // レビュー情報を取得
-//             const fetchMyReviewed = async () => {
-//                 try {
-//                     const response = await axios.get('/api/reviewed-purchases', {
-//                         headers: {
-//                             Authorization: `Bearer ${sessionStorage.getItem('auth_token')}`
-//                         }
-//                     });
-//                     const sortedReviewed = response.data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
-//                     const recentReviewed = sortedReviewed.slice(0, 5);
-//                     setReviewed(recentReviewed);
-//                     console.log('Fetched Reviewed:', recentReviewed);
-//                 } catch (error) {
-//                     console.error('Error fetching Reviewed:', error);
-//                 }
-//             };
-
-//         // カテゴリ情報を取得
-//         const fetchCategories = async () => {
-//             try {
-//                 const response = await axios.get("/api/categories");
-//                 const categoriesMap = response.data.reduce((map, category) => {
-//                     map[category.id] = category.name;
-//                     return map;
-//                 }, {});
-//                 setCategories(categoriesMap);
-//                 console.log("Fetched categories:", categoriesMap);
-//             } catch (error) {
-//                 console.error("Error fetching categories:", error);
-//             }
-//         };
-
-//         useEffect(() => {
-//             fetchUser();
-//             fetchCategories();
-//             fetchMyReviewed();
-//             // fetchMyPurchases();
-//         }, []);
-
-//         const handleDetailClick = (ideaId) => {
-//             // 詳細ボタンがクリックされたときの処理
-//             navigate(`/idea-detail/${ideaId}`); // `/idea-detail/:ideaId` のパスに遷移
-//         };
-
-//     return (
-//         <div>
-//             <Header />
-//             <main className="container">
-//                 <section className="section-container">
-//                     <h2>レビューしたアイディア一覧</h2>
-
-//                     {reviewed.length > 0 ? (
-//                         reviewed.map((review, index) => {
-//                             const { idea } = review;
-//                             return (
-//                                 <div className="idea-card" key={index}>
-//                                     <div className="idea-card__content">
-//                                         <div className="idea-card__title-category">
-//                                             <h3 className="idea-card__title">
-//                                                 {idea.title}
-//                                             </h3>
-//                                         </div>
-//                                         <p className="idea-card__summary">
-//                                             {idea.overview ||
-//                                                 "概要はありません"}
-//                                         </p>
-//                                         {/* <p className="purchase-card__summary">{purchase.review}</p> */}
-//                                         <div className="idea-card__meta">
-//                                             <span className="idea-card__review-count">
-//                                                 <i className="fa-regular fa-comment-dots"></i>
-//                                                 {idea.favorite_count || 0}
-//                                             </span>
-//                                             <span className="idea-card__average-rating">
-//                                                 <i className="fa-regular fa-thumbs-up"></i>
-//                                                 {idea.average_rating || 0}
-//                                             </span>
-//                                             <p className="idea-card__category">
-//                                                 <i className="fa-solid fa-tags"></i>
-//                                                 {categories[idea.category_id]}
-//                                             </p>
-//                                             {/* <span className="idea-card__rating">
-//                                             <i className="fa-regular fa-thumbs-up"></i>
-//                                             {purchase.rating}
-//                                         </span>
-//                                         <p className="purchase-card__created_date">
-//                                             <i className="fa-regular fa-clock"></i>
-//                                             {new Date(purchase.created_at).toLocaleDateString()}
-//                                         </p> */}
-//                                         </div>
-//                                     </div>
-//                                     <div className="idea-card__buttons">
-//                                         <button
-//                                             className="idea-card__button"
-//                                             onClick={() =>
-//                                                 handleDetailClick(idea.id)
-//                                             }
-//                                         >
-//                                             アイディアの詳細
-//                                         </button>
-//                                         <button
-//                                             className="idea-card__button"
-//                                             onClick={() =>
-//                                                 handleEditReviewClick(idea.id)
-//                                             }
-//                                         >
-//                                             レビューを編集
-//                                         </button>
-//                                     </div>
-//                                 </div>
-//                             );
-//                         })
-//                     ) : (
-//                         <p>購入はまだありません。</p>
-//                     )}
-//                 </section>
-//                 <button
-//                     className="idea-card__button"
-//                     onClick={() => navigate(-1)}
-//                 >
-//                     戻る
-//                 </button>
-//             </main>
-//             <Footer />
-//         </div>
-//     );
-
-// }
-
-// export default MyReviewHistory;
-
-// src/pages/MyReviewsList.js
 
 
 
@@ -17734,15 +17519,13 @@ function ReviewsList() {
       }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx)("section", {
         className: "section-container",
         children: reviews.length > 0 ? reviews.map(function (review, index) {
-          var _review$buyer;
           var isOwner = currentUser && review.buyer_id === currentUser.id;
           return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx)(_components_ReviewCard__WEBPACK_IMPORTED_MODULE_4__["default"], {
             idea: review.idea,
             review: review,
             user: {
-              name: ((_review$buyer = review.buyer) === null || _review$buyer === void 0 ? void 0 : _review$buyer.name) || '匿名ユーザー'
-            } // buyer の名前を表示
-            ,
+              name: review.buyer_id || '匿名ユーザー'
+            },
             isOwner: isOwner,
             buttons: [{
               label: "詳細",
@@ -17750,7 +17533,7 @@ function ReviewsList() {
                 return handleDetailClick(review.idea.id);
               }
             }, isOwner && {
-              label: "レビューを編集",
+              label: "編集",
               onClick: function onClick() {
                 return handleEditReviewClick(review.id);
               }
