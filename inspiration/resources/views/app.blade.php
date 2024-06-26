e2e02dff3b70e6fe059c8b3e91fc65d319dc4138
ti202206 <joker999joker@yahoo.co.jp>
2024-06-26 22:41:58 +0900

    ローカルの変更をコミット

--- inspiration/resources/views/app.blade.php
+++ inspiration/resources/views/app.blade.php
@@ -6,12 +6,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <link rel="icon" href="/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
-    <link rel="icon" href="{{ asset('assets/images/favicon.png') }}" type="image/png">
-    <link rel="apple-touch-icon" href="{{ asset('assets/images/apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon-16x16.png') }}">
+    <link rel="icon" href="https://tests-dev.net/assets/images/favicon.png">
+    <link rel="icon" href="https://tests-dev.net/favicon.ico">
+    <link rel="icon" href="https://tests-dev.net/assets/images/favicon-32x32.png">
+    <link rel="icon" href="https://tests-dev.net/assets/images/favicon-16x16.png">
+
 
     <script src="https://kit.fontawesome.com/719016ddac.js" crossorigin="anonymous"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
